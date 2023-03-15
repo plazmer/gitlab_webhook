5a18d73fcd73b168db62eb31b23bd8cf630e95a7
Plazmer <praw2003@gmail.com>
2023-03-15 09:21:29 +0500

    Bcghfdbk логику

--- db.py
+++ db.py
@@ -2,6 +2,6 @@ settings = {}
 
 for row in open('settings.tsv', 'r'):
     repo, email, tg_id = row.split("\t")
-    if not settings.get("repo"):
-        settings["repo"] = []
-    settings["repo"].append(tg_id)
+    if not settings.get(repo):
+        settings[repo] = []
+    settings[repo].append(tg_id)
